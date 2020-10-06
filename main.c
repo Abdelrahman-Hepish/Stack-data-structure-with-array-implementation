9b5ba5eb4bfd6119e9ade1c1e8d9842007da9e8f
7aposha <abdohepish13@gmail.com>
2020-10-06 11:15:08 +0200

    Documenting the Stack.h

--- main.c
+++ main.c
@@ -3,15 +3,25 @@
 void Display (stack_entry element ) ;
 int main() {
     Stack s ;
+    stack_entry element ;
     Creat_Stack(&s) ;
-    for(int i = 0 ; i <= 10 ; i++)
+    char message [100] ;
+    scanf("%s",message) ;
+    Push(message[0],&s);
+    for(int i = 0 ; message [i] != '\0' ; ++i)
     {
-        Push(i , &s) ;
+        if(message[i] != Stack_Top(&s))
+        {
+            Push (message[i],&s);
+        }else
+        {
+            Pop(&element , &s) ;
+        }
     }
     Travers_Stack(&s , Display) ;
     return 0;
 }
 void Display (stack_entry element )
 {
-    printf("the element is : %d \n",element) ;
+    printf("the element is : %c \n",element) ;
 }
--- stack.h
+++ stack.h
@@ -1,7 +1,12 @@
 #ifndef __STACK_H__
 #define __STACK_H__
-
-#define stack_entry int
+/*
+ * stack_entry is the data type of each element in the Stack the user selects it
+*/
+#define stack_entry char
+/*
+ * MAX_STACK is a macro which the user had to use to select the size of the stack
+ */
 #define MAX_STACK    100
 
 typedef int _bool ;
@@ -12,14 +17,59 @@ typedef struct
 }Stack ;
 typedef void (*funptr) (stack_entry element);
 
+/*
+ * Description : Creat_Stack is a function that initializes the Stack.
+ * return      : No thing.
+ * Complexity  : O(1).
+ */
 void Creat_Stack (Stack *) ;
+/*
+ * Description : Push is a function that puts a new element in the stack and increase it's size.
+ * return      : No thing.
+ * Complexity  : O(1).
+ */
 void Push (stack_entry , Stack * ) ;
+/*
+ * Description : Pop is a function that removes one element from the top of the stack and transfer it's value to the first parameter.
+ * return      : No thing.
+ * Complexity  : O(1).
+ */
 void Pop  (stack_entry * , Stack * ) ;
+/*
+ * Description : Stack_Full is a function that Checks if the Stack is full or not.
+ * return      : 1 if Stack is full and 0 if not full.
+ * Complexity  : O(1).
+ */
 _bool Stack_Full (Stack * ) ;
+/*
+ * Description : Stack_Empty is a function that Checks if the Stack is embty or not.
+ * return      : 1 if Stack is empty and 0 if not empty.
+ * Complexity  : O(1).
+ */
 _bool Stack_Empty (Stack *) ;
+/*
+ * Description : Stack_Top is a function that returns a copy from the element at the top of the Stack.
+ * return      : a value from data type stack_entry this value was at the top of the stack.
+ * Complexity  : O(1).
+ */
 stack_entry Stack_Top (Stack *) ;
+/*
+ * Description : Stack_Size is a function that returns the number of elements in the Stack.
+ * return      : the size of the Stack.
+ * Complexity  : O(1).
+ */
 int Stack_Size (Stack *) ;
+/*
+ * Description : Clear_Stack is a function that destroys all element in the Stack and makes it as it was initialized.
+ * return      : No thing.
+ * Complexity  : O(1).
+ */
 void Clear_Stack (Stack *) ;
+/*
+ * Description : Travers_Stack is a function that pass on all values in the Stack and pass the element to the passed function in the parameters.
+ * return      : No thing.
+ * Complexity  : O(N) as N : is the number of elements in the Stack.
+ */
 void Travers_Stack (Stack * ,funptr ) ;
 
 
